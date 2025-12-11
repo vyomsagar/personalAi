fd9f19897e47b1855681551aa45407e866d44a09
vyom sagar <vyom3103@gmail.com>
2025-12-11 14:32:48 +0530

    correction in names

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "frontend"))); // Serve frontend
+app.use(express.static(path.join(__dirname, "frontEnd"))); // Serve frontend
 const PORT = 3001;
 // let aiOutput = "";
 
@@ -37,7 +37,7 @@ app.post('/chat', async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+    res.sendFile(path.join(__dirname, "frontEnd", "index.html"));
 });
 
 
